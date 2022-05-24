dfd45521e171fb8f18f2e4284890bbc1ffe1fa54
Bowen Yang <bowenyang@MacBook-Pro-2.local>
2022-05-25 01:58:08 +1000

    add search bar

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,12 +1,24 @@
+import React, {useState} from "react";
+import {View, Text, StyleSheet} from "react-native";
+import SearchBar from "../components/SearchBar";
 
-import {View, Text} from "react-native";
+
 
 const SearchScreen = () => {
+    const [searchText, setSearchText] = useState("");
     return (
-        <View>
-            <Text>Search Screen</Text>
+        <View style={styles.container}>
+            <SearchBar searchText={searchText} setSearchText={setSearchText} />
+            <Text>{searchText}</Text>
         </View>
     )
 }
 
 export default SearchScreen;
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: "#fff",
+    }
+})
